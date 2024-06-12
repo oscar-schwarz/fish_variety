50b9318583af27760e6657f68830ff08c63411e0
oscar-schwarz <121044740+oscar-schwarz@users.noreply.github.com>
2024-06-12 15:19:21 +0000

    Deploying to javadoc from @ oscar-schwarz/fish_variety@8ec74ca4ae29627ee0c294b080e69b5b271c511a 🚀

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fish_variety","l":"Fish"},{"p":"fish_variety","l":"FishCollection"},{"p":"fish_variety","l":"FishConstraints"},{"p":"fish_variety","l":"FishPair"},{"p":"fish_variety","l":"Main"},{"p":"fish_variety","l":"Pair"}];updateSearchResults();

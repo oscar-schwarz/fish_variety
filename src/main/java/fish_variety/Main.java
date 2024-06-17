c569901ee27ea630d8ad4d78603b4536a9e65fd7
Oscar Schwarz <schwarz.oscar@protonmail.com>
2024-06-17 14:19:34 +0200

    fix(Main): output to console

--- src/main/java/fish_variety/Main.java
+++ src/main/java/fish_variety/Main.java
@@ -53,7 +53,7 @@ public class Main
             grueneMigraene, prachtpiranha, zitterling
         });
 
-        FishCollection bestVariant = FishCalculator.mostCompatibleFishTypesOfCollection(1000000, new FishCollection(constraints));
-        System.out.println(bestVariant);
+        FishCollection bestVariant = FishCalculator.mostCompatibleFishTypesOfCollection(170, new FishCollection(constraints));
+        System.out.println("Es koennen maximal " + bestVariant.getFishes().size() + " Fische zusammen gekauft werden, zum Beispiel: " + bestVariant.toString());
     }
 }
